270fcfe1b1d1f7b6f62bb7ed8cba1e33807c74f4
Denis Gridnev <denis.gridnev@gmail.com>
2015-06-01 11:02:57 +0300

    Создал новый проект

---
+++ FarmApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FarmApp.Startup))]
+namespace FarmApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
