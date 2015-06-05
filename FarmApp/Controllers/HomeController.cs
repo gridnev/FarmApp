a51e2ec46cb9b6e205e19c9ef2a17a838f0d3f8c
Denis Gridnev <denis.gridnev@gmail.com>
2015-06-05 14:06:53 +0300

    Главная страница, поиск

--- FarmApp/Controllers/HomeController.cs
+++ FarmApp/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FarmApp.Models;
+using FarmApp.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace FarmApp.Controllers
 {
@@ -10,7 +13,44 @@ namespace FarmApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            using (ProductDbContext ctx = ProductDbContext.Create())
+            {
+                var products = ctx.Products.Select(p => new ProductViewModel()
+                {
+                    Description = p.Description,
+                    Title = p.Title,
+                    Type = p.Type,
+                    Id = p.Id
+                }).ToList();
+
+                return View(products);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Search(string query)
+        {
+            query = query.Trim();
+            
+            if (string.IsNullOrEmpty(query))
+            {
+                return RedirectToAction("Index");
+            }
+
+            using (ProductDbContext ctx = ProductDbContext.Create())
+            {
+                var products = ctx.Products.Where(p => p.Title.Contains(query)).Select(p => new ProductViewModel()
+                {
+                    Description = p.Description,
+                    Title = p.Title,
+                    Type = p.Type,
+                    Id = p.Id
+                }).ToList();
+
+                ViewBag.Query = query;
+
+                return View("Index", products);
+            }
         }
 
         public ActionResult About()
